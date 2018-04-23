9f8e796b90721d3f2fa495d0c980e972a7e9277a
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace CustomFormFieldsReminder {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            DevExpress.XtraScheduler.TimeRuler timeRuler1 = new DevExpress.XtraScheduler.TimeRuler();
+            DevExpress.XtraScheduler.TimeRuler timeRuler2 = new DevExpress.XtraScheduler.TimeRuler();
+            this.schedulerControl1 = new DevExpress.XtraScheduler.SchedulerControl();
+            this.schedulerStorage1 = new DevExpress.XtraScheduler.SchedulerStorage(this.components);
+            this.carSchedulingBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.carsDBDataSet = new CustomFormFieldsReminder.CarsDBDataSet();
+            this.carsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.memoAptInfo = new DevExpress.XtraEditors.MemoEdit();
+            this.btnCreateAppReminder = new DevExpress.XtraEditors.SimpleButton();
+            this.carSchedulingTableAdapter = new CustomFormFieldsReminder.CarsDBDataSetTableAdapters.CarSchedulingTableAdapter();
+            this.carsTableAdapter = new CustomFormFieldsReminder.CarsDBDataSetTableAdapters.CarsTableAdapter();
+            this.defaultLookAndFeel1 = new DevExpress.LookAndFeel.DefaultLookAndFeel(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerStorage1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carSchedulingBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carsDBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.memoAptInfo.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // schedulerControl1
+            // 
+            this.schedulerControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.schedulerControl1.Location = new System.Drawing.Point(0, 57);
+            this.schedulerControl1.Name = "schedulerControl1";
+            this.schedulerControl1.Size = new System.Drawing.Size(707, 322);
+            this.schedulerControl1.Start = new System.DateTime(2008, 7, 14, 0, 0, 0, 0);
+            this.schedulerControl1.Storage = this.schedulerStorage1;
+            this.schedulerControl1.TabIndex = 0;
+            this.schedulerControl1.Text = "schedulerControl1";
+            this.schedulerControl1.Views.DayView.TimeRulers.Add(timeRuler1);
+            this.schedulerControl1.Views.WorkWeekView.TimeRulers.Add(timeRuler2);
+            this.schedulerControl1.EditAppointmentFormShowing += new DevExpress.XtraScheduler.AppointmentFormEventHandler(this.schedulerControl1_EditAppointmentFormShowing);
+            this.schedulerControl1.AppointmentViewInfoCustomizing += new DevExpress.XtraScheduler.AppointmentViewInfoCustomizingEventHandler(this.schedulerControl1_AppointmentViewInfoCustomizing);
+            // 
+            // schedulerStorage1
+            // 
+            this.schedulerStorage1.Appointments.CustomFieldMappings.Add(new DevExpress.XtraScheduler.AppointmentCustomFieldMapping("CustomPrice", "Price"));
+            this.schedulerStorage1.Appointments.DataSource = this.carSchedulingBindingSource;
+            this.schedulerStorage1.Appointments.Mappings.AllDay = "AllDay";
+            this.schedulerStorage1.Appointments.Mappings.Description = "Description";
+            this.schedulerStorage1.Appointments.Mappings.End = "EndTime";
+            this.schedulerStorage1.Appointments.Mappings.Label = "Label";
+            this.schedulerStorage1.Appointments.Mappings.Location = "Location";
+            this.schedulerStorage1.Appointments.Mappings.RecurrenceInfo = "RecurrenceInfo";
+            this.schedulerStorage1.Appointments.Mappings.ReminderInfo = "ReminderInfo";
+            this.schedulerStorage1.Appointments.Mappings.ResourceId = "CarId";
+            this.schedulerStorage1.Appointments.Mappings.Start = "StartTime";
+            this.schedulerStorage1.Appointments.Mappings.Status = "Status";
+            this.schedulerStorage1.Appointments.Mappings.Subject = "Subject";
+            this.schedulerStorage1.Appointments.Mappings.Type = "EventType";
+            this.schedulerStorage1.Resources.DataSource = this.carsBindingSource;
+            this.schedulerStorage1.Resources.Mappings.Caption = "Model";
+            this.schedulerStorage1.Resources.Mappings.Id = "ID";
+            this.schedulerStorage1.Resources.Mappings.Image = "Picture";
+            this.schedulerStorage1.ReminderAlert += new DevExpress.XtraScheduler.ReminderEventHandler(this.schedulerStorage1_ReminderAlert);
+            this.schedulerStorage1.AppointmentsChanged += new DevExpress.XtraScheduler.PersistentObjectsEventHandler(this.schedulerStorage1_AppointmentsChanged);
+            this.schedulerStorage1.AppointmentsInserted += new DevExpress.XtraScheduler.PersistentObjectsEventHandler(this.schedulerStorage1_AppointmentsChanged);
+            this.schedulerStorage1.AppointmentsDeleted += new DevExpress.XtraScheduler.PersistentObjectsEventHandler(this.schedulerStorage1_AppointmentsChanged);
+            // 
+            // carSchedulingBindingSource
+            // 
+            this.carSchedulingBindingSource.DataMember = "CarScheduling";
+            this.carSchedulingBindingSource.DataSource = this.carsDBDataSet;
+            // 
+            // carsDBDataSet
+            // 
+            this.carsDBDataSet.DataSetName = "CarsDBDataSet";
+            this.carsDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // carsBindingSource
+            // 
+            this.carsBindingSource.DataMember = "Cars";
+            this.carsBindingSource.DataSource = this.carsDBDataSet;
+            // 
+            // panelControl1
+            // 
+            this.panelControl1.Controls.Add(this.memoAptInfo);
+            this.panelControl1.Controls.Add(this.btnCreateAppReminder);
+            this.panelControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelControl1.Location = new System.Drawing.Point(0, 0);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(707, 57);
+            this.panelControl1.TabIndex = 1;
+            // 
+            // memoAptInfo
+            // 
+            this.memoAptInfo.Dock = System.Windows.Forms.DockStyle.Right;
+            this.memoAptInfo.Location = new System.Drawing.Point(262, 2);
+            this.memoAptInfo.Name = "memoAptInfo";
+            this.memoAptInfo.Size = new System.Drawing.Size(443, 53);
+            this.memoAptInfo.TabIndex = 1;
+            // 
+            // btnCreateAppReminder
+            // 
+            this.btnCreateAppReminder.Location = new System.Drawing.Point(12, 12);
+            this.btnCreateAppReminder.Name = "btnCreateAppReminder";
+            this.btnCreateAppReminder.Size = new System.Drawing.Size(215, 23);
+            this.btnCreateAppReminder.TabIndex = 0;
+            this.btnCreateAppReminder.Text = "Create Appointment Series with Reminder";
+            this.btnCreateAppReminder.Click += new System.EventHandler(this.btnCreateAppReminder_Click);
+            // 
+            // carSchedulingTableAdapter
+            // 
+            this.carSchedulingTableAdapter.ClearBeforeFill = true;
+            // 
+            // carsTableAdapter
+            // 
+            this.carsTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(707, 379);
+            this.Controls.Add(this.schedulerControl1);
+            this.Controls.Add(this.panelControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schedulerStorage1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carSchedulingBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carsDBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.carsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.memoAptInfo.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraScheduler.SchedulerControl schedulerControl1;
+        private DevExpress.XtraScheduler.SchedulerStorage schedulerStorage1;
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private CarsDBDataSet carsDBDataSet;
+        private System.Windows.Forms.BindingSource carSchedulingBindingSource;
+        private CustomFormFieldsReminder.CarsDBDataSetTableAdapters.CarSchedulingTableAdapter carSchedulingTableAdapter;
+        private System.Windows.Forms.BindingSource carsBindingSource;
+        private CustomFormFieldsReminder.CarsDBDataSetTableAdapters.CarsTableAdapter carsTableAdapter;
+        private DevExpress.XtraEditors.SimpleButton btnCreateAppReminder;
+        private DevExpress.LookAndFeel.DefaultLookAndFeel defaultLookAndFeel1;
+        private DevExpress.XtraEditors.MemoEdit memoAptInfo;
+    }
+}
+
---
+++ CS/Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraScheduler;
+using DevExpress.XtraEditors;
+using System.Data.OleDb;
+
+namespace CustomFormFieldsReminder {
+    public partial class Form1 : XtraForm {
+        public Form1() {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+            schedulerControl1.SelectedAppointments.CollectionChanged+=new DevExpress.Utils.CollectionChangedEventHandler<Appointment>(SelectedAppointments_CollectionChanged);
+
+            schedulerControl1.OptionsBehavior.ShowRemindersForm = false;
+            schedulerControl1.OptionsBehavior.RemindersFormDefaultAction = RemindersFormDefaultAction.Custom;
+
+            schedulerControl1.ActiveViewType = SchedulerViewType.Day;
+            schedulerControl1.DayView.DayCount = 2;
+            schedulerControl1.DayView.ResourcesPerPage = 2;
+            schedulerControl1.GroupType = SchedulerGroupType.Resource;
+            schedulerControl1.OptionsView.ResourceHeaders.ImageSize = new Size(100, 50);
+            schedulerControl1.OptionsView.ResourceHeaders.ImageSizeMode = HeaderImageSizeMode.ZoomImage;
+
+            schedulerControl1.Start = DateTime.Today;
+            
+            // TODO: This line of code loads data into the 'carsDBDataSet.Cars' table. You can move, or remove it, as needed.
+            this.carsTableAdapter.Fill(this.carsDBDataSet.Cars);
+            // TODO: This line of code loads data into the 'carsDBDataSet.CarScheduling' table. You can move, or remove it, as needed.
+            this.carSchedulingTableAdapter.Fill(this.carsDBDataSet.CarScheduling);
+            carSchedulingTableAdapter.Adapter.RowUpdated += new OleDbRowUpdatedEventHandler(carSchedulingTableAdapter_RowUpdated);
+
+        }
+
+
+
+        private void schedulerControl1_EditAppointmentFormShowing(object sender, DevExpress.XtraScheduler.AppointmentFormEventArgs e) {
+            Appointment apt = e.Appointment;
+            bool openRecurrenceForm = apt.IsRecurring && schedulerStorage1.Appointments.IsNewAppointment(apt);
+
+            MyAppointmentEditForm f = new MyAppointmentEditForm((SchedulerControl)sender, apt, openRecurrenceForm);
+            try {
+                f.LookAndFeel.ParentLookAndFeel = this.LookAndFeel.ParentLookAndFeel;
+                e.DialogResult = f.ShowDialog();
+                e.Handled = true;
+
+                if (apt.Type == AppointmentType.Pattern && schedulerControl1.SelectedAppointments.Contains(apt))
+                    schedulerControl1.SelectedAppointments.Remove(apt);
+
+                schedulerControl1.Refresh();
+            }
+            finally {
+            f.Dispose();
+            }
+        }
+
+        private void schedulerStorage1_AppointmentsChanged(object sender, PersistentObjectsEventArgs e) {
+            carSchedulingTableAdapter.Update(this.carsDBDataSet);
+            this.carsDBDataSet.AcceptChanges();
+        }
+
+        private void carSchedulingTableAdapter_RowUpdated(object sender, OleDbRowUpdatedEventArgs e) {
+            if(e.Status == UpdateStatus.Continue && e.StatementType == StatementType.Insert) {
+                int id = 0;
+                using(OleDbCommand cmd = new OleDbCommand("SELECT @@IDENTITY", carSchedulingTableAdapter.Connection)) {
+                    id = (int)cmd.ExecuteScalar();
+                }
+                e.Row["ID"] = id;
+            }
+        }
+        private void btnCreateAppReminder_Click(object sender, EventArgs e) {
+            DateTime nowtime = DateTime.Now.AddMinutes(16);
+            Appointment aptPattern = schedulerStorage1.CreateAppointment(AppointmentType.Pattern);
+            aptPattern.Subject = "Appointment with Reminder";
+            aptPattern.Description = "Recurrence Appointment with reminder";
+            aptPattern.Duration = TimeSpan.FromHours(2);
+            aptPattern.ResourceId = schedulerStorage1.Resources[0].Id;
+            aptPattern.StatusId = (int)AppointmentStatusType.Busy;
+            aptPattern.LabelId = 1;
+            aptPattern.CustomFields["CustomPrice"] = (decimal)15.25;
+
+            aptPattern.RecurrenceInfo.Type = RecurrenceType.Daily;
+            aptPattern.RecurrenceInfo.Periodicity = 2;
+            aptPattern.RecurrenceInfo.Range = RecurrenceRange.OccurrenceCount;
+            aptPattern.RecurrenceInfo.OccurrenceCount = 10;
+            aptPattern.RecurrenceInfo.Start = nowtime.AddDays(-4);
+
+            aptPattern.HasReminder = true;
+            aptPattern.Reminder.TimeBeforeStart = TimeSpan.FromMinutes(15);
+
+
+            schedulerStorage1.Appointments.Add(aptPattern);
+        }
+
+        private void schedulerStorage1_ReminderAlert(object sender, ReminderEventArgs e) {
+            // Create a new appointment
+            Appointment app = schedulerStorage1.CreateAppointment(AppointmentType.Normal);
+            app.Subject = "Created on alert from appointment w/Price = " + e.AlertNotifications[0].ActualAppointment.CustomFields["CustomPrice"];
+            app.Start = e.AlertNotifications[0].ActualAppointment.Start.AddHours(2);
+            app.Duration = TimeSpan.FromHours(4);
+            schedulerStorage1.Appointments.Add(app);
+
+            // Modify the appointment for which the alert is triggered
+            e.AlertNotifications[0].ActualAppointment.LabelId = 3;
+
+            // Prevent the event from being fired one more time
+            e.AlertNotifications[0].ActualAppointment.Reminder.Dismiss();
+        }
+
+        private void schedulerControl1_AppointmentViewInfoCustomizing(object sender, AppointmentViewInfoCustomizingEventArgs e) {
+            Appointment apt = e.ViewInfo.Appointment;
+            if (apt.HasReminder && apt.Type == AppointmentType.Occurrence && apt.RecurrencePattern != null) {
+                Appointment pattern = apt.RecurrencePattern;
+                RecurringReminder reminder = (RecurringReminder)pattern.Reminder;
+                e.ViewInfo.ShowBell = reminder.AlertOccurrenceIndex <= apt.RecurrenceIndex;
+            }
+        }
+
+
+        void SelectedAppointments_CollectionChanged(object sender, DevExpress.Utils.CollectionChangedEventArgs<Appointment> e) {
+            memoAptInfo.Text = "";
+            if (schedulerControl1.SelectedAppointments.Count == 0) return;
+
+            Appointment apt = schedulerControl1.SelectedAppointments[0];            
+            if (apt != null && apt.IsRecurring) {
+                Appointment nextOcc = apt.RecurrencePattern.GetOccurrence(apt.RecurrenceIndex + 1);
+                memoAptInfo.Text = "The next occurrence has index " + nextOcc.RecurrenceIndex + " and Price=" + nextOcc.CustomFields["CustomPrice"]
+                    + "\r\n";
+                if (apt.HasReminder) {
+                    memoAptInfo.Text += "The reminder alert starts at " + apt.Reminder.AlertTime;                
+                }
+
+               }
+            
+
+
+
+        }
+       
+    }
+}
